6d23404e9491354d854c8577b1aa8c9c50a151a1
Pratik-Coder123 <pratikghule8329@gmail.com>
2022-09-06 13:35:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const signupbtn= document.querySelector(".signupbtn")
+const loginbtn= document.querySelector(".loginbtn")
+const movebtn= document.querySelector(".movebtn")
+const signup=document.querySelector(".signup")
+const login=document.querySelector(".login")
+
+loginbtn.addEventListener("click",()=>{
+movebtn.classList.add("rightbtn")
+login.classList.add("loginform")
+ signup.classList.remove("signupform")
+
+
+})
+
+signupbtn.addEventListener("click",()=>{
+    movebtn.classList.remove("rightbtn")
+    login.classList.remove("loginform")
+ signup.classList.add("signupform")
+    
+    })
